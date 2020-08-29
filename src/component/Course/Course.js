5701295cd75c9381bcb5170c82c3e7cb8823c9d8
sayeedreza99 <sayeedreza99.com>
2020-08-29 21:50:41 +0600

    bootstrap for col-row added

--- src/component/Course/Course.js
+++ src/component/Course/Course.js
@@ -7,19 +7,23 @@ const Course = (props) => {
     // console.log(props);
     const { name, img, mentor, rating, price } = props.course;
     const btnStyle = { color: 'white', backgroundColor: 'goldenrod', width: '150px', height: '35px' };
-    const imgStyle = { width: '25%', marginRight: '10px', padding: '10px' };
+    const imgStyle = { marginRight: '10px', padding: '10px' };
     return (
-        <div style={{ display: 'flex', borderBottom: '1px solid lightgray' }}>
-            <img style={imgStyle} src={img} alt="" />
-            <div style={{ width: '60%' }}>
-                <h2>Course Name: {name}</h2>
-                <h4>Instructor: {mentor}</h4>
-                <p><small> Rating: {rating}</small></p>
-                <h4>Price: ${price}</h4>
-                <button class="btn btn-warning" style={btnStyle} onClick={() => props.handleAddCourse(props.course)}> <FontAwesomeIcon icon={faPlus} />Enroll now</button>
-            </div>
+        <div className='container' style={{ borderBottom: '1px solid lightgray' }}>
+            <div className="row">
+                <div className='col-md-4'>
+                    <img style={imgStyle} src={img} alt="" />
+                </div>
+                <div className='col-md-8'>
+                    <h4>Course Name: {name}</h4>
+                    <h5>Instructor: {mentor}</h5>
+                    <p><small> Rating: {rating}</small></p>
+                    <h4>Price: ${price}</h4>
+                    <button class="btn btn-warning" style={btnStyle} onClick={() => props.handleAddCourse(props.course)}> <FontAwesomeIcon icon={faPlus} />Enroll now</button>
 
-        </div >
+                </div>
+            </div>
+        </div>
     );
 };
 
